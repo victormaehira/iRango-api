950fb530225463b0a1b8f6c890daad21bc3372e6
victor.maehira <vymaehira@gmail.com>
2021-08-23 13:05:37 -0300

    Spring boot added.

---
+++ src/main/java/com/victor/lab/irango/IrangoApplication.java
@@ -0,0 +1,13 @@
+package com.victor.lab.irango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/lab/irango/IrangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.lab.irango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
