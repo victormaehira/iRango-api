92519509809afcf1f17417887ac8a64a9b5d2660
Victor Yuji Maehira <victor.maehira@certintra.com.br>
2021-10-10 20:05:37 -0300

    Problem details exposed

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -19,5 +20,15 @@ public class Problem {
 	private String title;
 	private String detail;
 	private String userMessage;
+	private List<Field> fields;
+	
+	@Getter
+	@Builder
+	public static class Field {
+		
+		private String name;
+		private String userMessage;
+		
+	}
 	
 }
