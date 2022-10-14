219a0e6f82604a49c5b9f3697237108a78894dd5
victor.maehira <vymaehira@gmail.com>
2022-10-14 16:28:03 -0300

    Integration test added.

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
@@ -37,6 +39,16 @@ public class CadastroCozinhaIntegrationTests {
 		novaCozinha.setNome(null);
 		
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+	}
+	
+	@Test(expected = EntidadeEmUsoException.class)
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		cadastroCozinha.excluir(1L);
+	}
+
+	@Test(expected = CozinhaNaoEncontradaException.class)
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		cadastroCozinha.excluir(100L);
 	}
 
 }
